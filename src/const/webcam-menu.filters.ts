86f4a3a8d1e7bd2b315ebaa8cec69cf28b22521b
nf <nicholas.facchin@finconsgroup.com>
2022-11-25 11:51:36 +0100

    lista webcam

--- src/const/webcam-menu.filters.ts
+++ src/const/webcam-menu.filters.ts
@@ -1,17 +1,32 @@
 import { MenuFilters, KeyboardButton } from "@quino/telegraf-menu";
 import fetch from "node-fetch";
 import { MenuAction } from "../interfaces/commands.models";
+import { IWebcam } from "../interfaces/webcam.models";
 
 export const WEBCAM_MENU_FILTERS: () => Promise<
   MenuFilters<MenuAction>[]
 > = async () => {
-  const res = await fetch(
-    `${process.env.API_URL}/o/map-rest/webcam/A4AAA`
+  const res = await fetch(`${process.env.API_URL}/o/map-rest/webcam/A4AAA`);
+  const json: IWebcam[] = await res.json();
+  console.log(json);
+
+  const listWebcamAttive = [
+    "TVCIO228-Ovest",
+    "TVCIO244-Ovest",
+    "TVCIE251-Est",
+    "TVCIO259-Ovest",
+    "TVCIO270-Ovest",
+    "TVCIE291-Est",
+    "TVCIE303-Est",
+    "TVCIO316-Ovest",
+    "TVCIE328-Est",
+    "TVCIE336-Est",
+    "TVCIE336-Est",
+  ];
+  const webcams = json.filter((item) => listWebcamAttive.includes(item.id));
+  const buttons = webcams.map(
+    (item) => new KeyboardButton(item.id, item.url) as any
   );
-  const json: any[] = await res.json();
-  const buttons = json
-    .splice(0, 6)
-    .map((item) => new KeyboardButton(item.name, item.url));
   const rowSize = 2;
   const rows = [];
   for (let i = 0; i < buttons.length; i += rowSize) {
---
+++ src/interfaces/webcam.models.ts
@@ -0,0 +1,8 @@
+export interface IWebcam {
+  lng: number;
+  name: string;
+  id: string;
+  lat: number;
+  url: string;
+  direction: string;
+}
